60a9f40bd5a5541ec9ea4d1dbf30560863b7e83f
pznamir00 <pznamir00@gmail.com>
2021-10-04 22:45:56 +0200

    Add url field to Driver Model

--- drivers/models.py
+++ drivers/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Platform(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+    url = models.URLField(max_length=1024)
     driver_class_name = models.CharField(max_length=32)
     created_at = models.DateTimeField(auto_created=True)
     
